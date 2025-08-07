11bb37bafd9e5f5f7f267a414721c0622abf9794
Eunah26 <kaueunah@gmail.com>
2025-08-07 23:39:03 +0400

    Add files via upload

---
+++ Emp_ReadFile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WageCal;
+
+/**
+ *
+ * @author Eunah Kau
+ */
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader; 
+
+
+public class Emp_ReadFile {
+    private String _path;
+    public Emp_ReadFile(String file_path){
+      _path=file_path;
+    }
+    
+   
